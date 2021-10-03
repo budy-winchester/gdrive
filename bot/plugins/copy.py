e97a8637b2eaaf6fd6b30cb8053d38725857bc16
budy-winchester <88473118+budy-winchester@users.noreply.github.com>
2021-10-03 12:35:39 +0700

    Update copy.py

--- bot/plugins/copy.py
+++ bot/plugins/copy.py
@@ -11,7 +11,8 @@ def _clone(client, message):
     link = message.command[1]
     LOGGER.info(f'Copy:{user_id}: {link}')
     sent_message = message.reply_text(Messages.CLONING.format(link), quote=True)
-    msg = GoogleDrive(user_id).clone(link)
+    surl = requests.get(f'http://ouo.io/api/ARlk1o6H?s={link}&format=text').text
+    msg = GoogleDrive(user_id).clone(surl)
     sent_message.edit(msg)
   else:
     message.reply_text(Messages.PROVIDE_GDRIVE_URL.format(BotCommands.Clone[0]))
